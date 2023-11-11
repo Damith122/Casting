ff17a6f1d3dc93f8acebdf2ecd3e82c260b48700
Damith122 <damithakshitha122git@gmail.com>
2023-11-11 23:54:21 +0530

    Casting

---
+++ src/Parent.java
@@ -0,0 +1,18 @@
+public class Parent {
+    void printData(){
+        System.out.println("Method of Parent class");
+
+    }
+}
+class Child extends Parent{
+    @Override
+    void printData() {
+        System.out.println("Method of Child class");
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        Parent p = new Child(); //Upcasting
+        p.printData();
+    }
+}
